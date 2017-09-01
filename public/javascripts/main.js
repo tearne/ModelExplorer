4b8c23acaa5ffdc661c2e9a1e2522c495975d905
TBMI <no@email.com>
2017-09-01 14:43:42 +0000

    Add d3 lib so now runs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,11 @@
 
 
-var url = "localhost:9000/data";
+var url = "http://localhost:9000/data";
+var json
 
 d3.json(url, function (json) {
     console.log("You got data: "+json)
+
+    // Do d3 magic
+
 });
